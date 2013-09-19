3beec341f33add548dae2b9e2aeeccddf2125cbe
funzeye <funzeye@gmail.com>
2013-09-19 15:57:39 +0100

    Updated gh-pages site

---
+++ js/MoveMap.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.county-map li').css('margin', '0 0 0 -20%');
+    	$('.county-map li').css('margin', '0 0 0 -30%');
+    	$('.county-map li').css('margin', '0 0 0 -50%');
+    	$('.county-map li').css('margin', '0 0 0 -97%'); 
+    	$('.county-map li').css('margin', '0 0 0 -98%'); 
+    	$('.county-map li').css('margin', '0 0 0 -99%'); 
+    	$('.county-map li').css('margin', '0 0 0 -100%');
+});
+    	 
+ 
